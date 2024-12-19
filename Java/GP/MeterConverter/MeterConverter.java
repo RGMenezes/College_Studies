88908ba0328d97528b0a2a0d5a1905fc61395b4b
Rafael Menezes <109706910+RGMenezes@users.noreply.github.com>
2024-12-18 21:09:50 -0300

    creating meter converter, 2 question

---
+++ Java/GP/MeterConverter/MeterConverter.java
@@ -0,0 +1,19 @@
+package Java.GP.MeterConverter;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class MeterConverter{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    PrintStream print = System.out;;
+
+    print.print("Escreva os metros a serem convertidos: ");
+    float numberMeter = input.nextFloat();
+
+    float numberCentimeter = numberMeter*100;
+
+    print.print("Temos então " + numberCentimeter + "cm.");
+
+    input.close();
+  }
+}
