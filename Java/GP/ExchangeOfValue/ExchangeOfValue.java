a1dfd89aa0990db57247498710aa17156af16c98
Rafael Menezes <109706910+RGMenezes@users.noreply.github.com>
2024-12-19 19:29:29 -0300

    Finished exchange of values

---
+++ Java/GP/ExchangeOfValue/ExchangeOfValue.java
@@ -0,0 +1,26 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class ExchangeOfValue{
+  public static void main(String[] args) {
+    int a,b,c;
+    Scanner input = new Scanner(System.in);
+    PrintStream print = System.out;
+
+    print.print("Escreva o número da variavel a: ");
+    a = input.nextInt();
+
+    print.print("Escreva o número da variavel b: ");
+    b = input.nextInt();
+    
+    print.println("Variavel a = " + a + ", variavel b = " + b);
+
+    c = a;
+    a = b;
+    b = c;
+
+    print.println("Variavel a = " + a + ", variavel b = " + b);
+
+    input.close();
+  }
+}
