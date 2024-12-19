56571512cfa511bebeab47807fa791403955f3be
Rafael Menezes <109706910+RGMenezes@users.noreply.github.com>
2024-12-19 19:40:24 -0300

    Finished space covered

---
+++ Java/GP/SpaceCovered/SpaceCovered.java
@@ -0,0 +1,27 @@
+package Java.GP.SpaceCovered;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class SpaceCovered {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    PrintStream print = System.out;
+    int So, V, T, result;
+
+    print.print("Qual o espaço inicial do objeto(m): ");
+    So = input.nextInt();
+
+    print.print("Qual a velocidade do objeto (m/s): ");
+    V = input.nextInt();
+
+    print.print("Qual o tempo de deslocamento do objeto(s): ");
+    T = input.nextInt();
+
+    result = So + V * T;
+
+    print.println("O espaço percorrido pelo objeto é de " + result + "m.");
+
+    input.close();
+  }
+}
