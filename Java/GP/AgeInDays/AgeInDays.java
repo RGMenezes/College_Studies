a89801984c6533595dc32c904ef1480583439fa7
Rafael Menezes <109706910+RGMenezes@users.noreply.github.com>
2024-12-19 23:13:39 -0300

    finish age in days question

---
+++ Java/GP/AgeInDays/AgeInDays.java
@@ -0,0 +1,43 @@
+package Java.GP.AgeInDays;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class AgeInDays {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    PrintStream print = System.out;
+    Age age = new Age(0, 0, 0);
+
+    print.println("Calculadora de dias de vida");
+
+    print.print("Quantos anos você tem? ");
+    age.year = input.nextInt();    
+
+    print.print("Quantos meses completos, apartir do seu último aniversário, você tem? ");
+    age.mount = input.nextInt();
+
+    print.print("E quantos dias? ");
+    age.day = input.nextInt();
+
+    print.println("Você tem " + age.ageDays() + " dias de vida.");
+
+    input.close();
+  }
+
+  public static class Age{
+    int year;
+    int mount;
+    int day;
+
+    public Age(int year, int mount, int day){
+      this.year = year;
+      this.mount = mount;
+      this.day = day;
+    }
+
+    public int ageDays(){
+      return year * 365 + mount * 30 + day;
+   }
+  }
+}
