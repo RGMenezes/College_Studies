ff105e825116674a9908ef04abf505f8f7f9afa6
Rafael Menezes <109706910+RGMenezes@users.noreply.github.com>
2024-12-19 22:39:34 -0300

    finish BMI calculator question

---
+++ Java/GP/BMICalculator/BMICalculator.java
@@ -0,0 +1,24 @@
+package Java.GP.BMICalculator;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class BMICalculator {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    PrintStream print = System.out;
+    double height, weight;
+
+    print.println("Calculadora IMC");
+
+    print.print("Qual a sua altura?");
+    height = input.nextDouble();    
+
+    print.print("Qual o sou peso?");
+    weight = input.nextDouble();
+
+    print.println("O sou índice de massa corporal é " + String.format("%.2f", weight / height * 2) + ".");
+
+    input.close();
+  }
+}
