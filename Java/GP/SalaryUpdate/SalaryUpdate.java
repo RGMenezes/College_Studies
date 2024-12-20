66eccadc3f9339d1f516ef91e2f0407d10c087aa
Rafael Menezes <109706910+RGMenezes@users.noreply.github.com>
2024-12-19 22:49:36 -0300

    finish salary update question

---
+++ Java/GP/SalaryUpdate/SalaryUpdate.java
@@ -0,0 +1,26 @@
+package Java.GP.SalaryUpdate;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class SalaryUpdate {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    PrintStream print = System.out;
+    double salary, readjustment, result;
+
+    print.println("Atualização de Salário");
+
+    print.print("Salário do funcionário: ");
+    salary = input.nextDouble();
+
+    print.print("Percentual de reajuste(%): ");
+    readjustment = input.nextDouble();
+
+    result = salary * (readjustment + 100) / 100;
+
+    print.println("O salário atualizado é R$" + String.format("%.2f", result));
+
+    input.close();
+  }
+}
