23866624bee4d5b8a8c3da6170d113bde9599d51
Rafael Menezes <109706910+RGMenezes@users.noreply.github.com>
2024-12-19 22:19:33 -0300

    finish note by weight question

---
+++ Java/GP/NoteByWeight/NoteByWeight.java
@@ -0,0 +1,42 @@
+package Java.GP.NoteByWeight;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class NoteByWeight {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    PrintStream print = System.out;
+    Notes notes = new Notes(0, 0, 0);
+
+    print.println("Calculadora de notas por peso.");
+    print.print("Nota 1: ");
+    notes.note1 = input.nextInt();
+
+    print.print("Nota 2: ");
+    notes.note2 = input.nextInt();
+
+    print.print("Nota 3: ");
+    notes.note3 = input.nextInt();
+
+    print.println("A média é: " + String.format("%.2f", notes.weightCalculate()));
+
+    input.close();
+  }
+
+  public static class Notes{
+    int note1;
+    int note2;
+    int note3;
+
+    public Notes(int note1, int note2, int note3){
+      this.note1 = note1;
+      this.note2 = note2;
+      this.note3 = note3;
+    }
+
+    public double weightCalculate(){
+      return (this.note1 + this.note2 * 2 + this.note3 * 3) / 6;
+    }
+  }
+}
