966458f79b7b5548bf9ddc06196ad1b1d920c9d6
Rafael Menezes <109706910+RGMenezes@users.noreply.github.com>
2024-12-20 00:37:35 -0300

    finish bank note calculator question 13

---
+++ Java/GP/BanknoteCalculator/BanknoteCalculator.java
@@ -0,0 +1,44 @@
+package Java.GP.BanknoteCalculator;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class BanknoteCalculator {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    PrintStream print = System.out;
+    int noteArray[] = {200, 100, 50, 20, 10, 5, 2, 1};
+    String noteString = "serão usadas as notas: ";
+    int value;
+
+    print.println("Calculador do mínimo de céludas necessárias");
+
+    print.print("Digite o valor total: ");
+    value = input.nextInt();
+
+    print.println("Para o valor de " + value + " " + noteQuantities(value, 0, noteArray, noteString));
+
+
+    input.close();
+  }
+
+  public static String noteQuantities(int value, int initialNote, int noteArray[], String noteString){
+    if(value == 0) return noteString;      
+    if(initialNote >= noteArray.length) return "Error: Não há notas menores para calcular!"; 
+    int banknote = noteArray[initialNote];
+    
+    if (value >= banknote) {
+      int leftovers = value%banknote;
+      int counter = (value - leftovers) / banknote;
+
+      noteString += banknote + " x" + counter;     
+
+      if (leftovers != 0) noteString += ", ";
+      if (leftovers == 0) noteString += ".";
+
+      return noteQuantities(leftovers, initialNote+1, noteArray, noteString);
+    }
+    
+    return noteQuantities(value, initialNote+1, noteArray, noteString);
+  }
+}
