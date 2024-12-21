10899c607d2e995ee8bb06c7d4edfe6875cefce2
Rafael Menezes <109706910+RGMenezes@users.noreply.github.com>
2024-12-21 12:48:21 -0300

    Finish negative number question 14

---
+++ Java/GP/Selection_Structures/NegativeNumber/NegativeNumber.java
@@ -0,0 +1,18 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class NegativeNumber {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    PrintStream print = System.out;
+    int number;
+
+    print.print("Escreva o número: ");
+    number = input.nextInt();
+
+    if(number < 0) print.println(number + " é negativo.");
+    else print.println(number + " é positivo.");
+
+    input.close();
+  }
+}
