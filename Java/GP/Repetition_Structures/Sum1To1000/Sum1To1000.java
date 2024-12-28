a66c03b84be7cb78aaa231d9224848c80f2ac45e
Rafael Menezes <109706910+RGMenezes@users.noreply.github.com>
2024-12-28 16:19:13 -0300

    Finish sum 1 to 1000 question 37

---
+++ Java/GP/Repetition_Structures/Sum1To1000/Sum1To1000.java
@@ -0,0 +1,22 @@
+package Java.GP.Repetition_Structures.Sum1To1000;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Sum1To1000 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    PrintStream print = System.out;
+    int loop;
+    int sum = 0;
+
+    print.print("Limite: ");
+    loop = input.nextInt();
+
+    for(int i = 1; i <= loop; i += 1) sum += i;
+
+    print.println(sum);
+
+    input.close();
+  }
+}
