0a87a089bb59cc5691934e6d28c2bcb8396e1da6
Rafael Menezes <109706910+RGMenezes@users.noreply.github.com>
2025-04-01 21:48:38 -0300

    continuando exercicios

---
+++ Java/LinguagemDeProgramacao/Vetores/Exercicio05/Exercicio05.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Exercicio05 {
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        Scanner entradaDoTeclado = new Scanner(System.in);
+        String listaDePessoas[] = new String[200];
+        int contadorMulher = 0;
+        int contadorHomem = listaDePessoas.length - 1;
+
+        while(contadorMulher <= contadorHomem){
+            char sexo;
+
+            do{
+                System.out.print("\nDigite o sexo: ");
+                sexo = entradaDoTeclado.nextLine().charAt(0);
+            } while(!(sexo == 'f' || sexo == 'F' || sexo == 'm' || sexo == 'M'));
+
+            System.out.print("Digite o nome: ");
+            if(sexo == 'f' || sexo == 'F'){
+                listaDePessoas[contadorMulher] = entradaDoTeclado.nextLine();
+                contadorMulher++;
+            }else{
+                listaDePessoas[contadorHomem] = entradaDoTeclado.nextLine();
+                contadorHomem--;
+            }
+        }
+
+        for (int i = 0; i < listaDePessoas.length; i++) {
+            System.out.println(listaDePessoas[i]);
+        }
+    }
+}
+
+// 5. Prepare um programa para perguntar o nome e o sexo de 200 pessoas. Vocˆe deve listar primeiro o nome de todas
+// as mulheres e depois o nome de todos os homens.
---
+++ Java/LinguagemDeProgramacao/Vetores/Exercicio06/Exercicio06.java
@@ -0,0 +1,16 @@
+package Java.LinguagemDeProgramacao.Vetores.Exercicio06;
+
+import java.util.Scanner;
+
+public class Exercicio06 {
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        Scanner entradaDoTeclado = new Scanner(System.in);
+        String listaNomes[] = new String[30];
+        int listaIdade[] = new int[listaNomes.length];
+
+    }
+}
+
+// 6. Prepare um programa para ler o nome e a idade de 30 pessoas. Ap´os a leitura dos dados o programa dever´a
+// informar o nome de todas as pessoas que ficarem acima da m´edia de idades.
