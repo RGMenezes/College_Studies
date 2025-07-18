d13b819709beba1c7777db28458e20ebfc126282
Rafael Menezes <109706910+RGMenezes@users.noreply.github.com>
2025-07-17 21:19:16 -0300

    iniciando projeto p2

---
+++ Java/ProgramacaoOO/ProjetoP2/Concessionaria/src/main/java/com/Concessionaria/app/App.java
@@ -0,0 +1,7 @@
+package Java.ProgramacaoOO.ProjetoP2.Concessionaria.src.main.java.com.Concessionaria.app;
+
+public class App {
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
